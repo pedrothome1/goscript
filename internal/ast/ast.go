caa84555f9fea1421df5a110d4392d9be73a5567
pedrothome1 <pedrothome2@outlook.com>
2024-11-02 19:51:11 -0300

    add ast acceptor generator

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -4,6 +4,8 @@ import (
 	"github.com/pedrothome1/goscript/internal/token"
 )
 
+//go:generate go run ../../cmd/gen/gen.go
+
 type ExprVisitor interface {
 	VisitIdent(expr *Ident) (any, error)
 	VisitEllipsis(expr *Ellipsis) (any, error)
@@ -49,6 +51,11 @@ type StmtVisitor interface {
 type Expr interface {
 	Accept(v ExprVisitor) (any, error)
 	exprNode()
+}
+
+type Stmt interface {
+	Accept(v StmtVisitor) error
+	stmtNode()
 }
 
 type Ident struct {
@@ -136,49 +143,7 @@ type FuncType struct {
 	Result *Field
 }
 
-func (e *Ident) Accept(v ExprVisitor) (any, error)         { return v.VisitIdent(e) }
-func (e *Ident) exprNode()                                 {}
-func (e *Ellipsis) Accept(v ExprVisitor) (any, error)      { return v.VisitEllipsis(e) }
-func (e *Ellipsis) exprNode()                              {}
-func (e *BasicLit) Accept(v ExprVisitor) (any, error)      { return v.VisitBasicLit(e) }
-func (e *BasicLit) exprNode()                              {}
-func (e *FuncLit) Accept(v ExprVisitor) (any, error)       { return v.VisitFuncLit(e) }
-func (e *FuncLit) exprNode()                               {}
-func (e *CompositeLit) Accept(v ExprVisitor) (any, error)  { return v.VisitCompositeLit(e) }
-func (e *CompositeLit) exprNode()                          {}
-func (e *BinaryExpr) Accept(v ExprVisitor) (any, error)    { return v.VisitBinaryExpr(e) }
-func (e *BinaryExpr) exprNode()                            {}
-func (e *UnaryExpr) Accept(v ExprVisitor) (any, error)     { return v.VisitUnaryExpr(e) }
-func (e *UnaryExpr) exprNode()                             {}
-func (e *CallExpr) Accept(v ExprVisitor) (any, error)      { return v.VisitCallExpr(e) }
-func (e *CallExpr) exprNode()                              {}
-func (e *ParenExpr) Accept(v ExprVisitor) (any, error)     { return v.VisitParenExpr(e) }
-func (e *ParenExpr) exprNode()                             {}
-func (e *SelectorExpr) Accept(v ExprVisitor) (any, error)  { return v.VisitSelectorExpr(e) }
-func (e *SelectorExpr) exprNode()                          {}
-func (e *IndexExpr) Accept(v ExprVisitor) (any, error)     { return v.VisitIndexExpr(e) }
-func (e *IndexExpr) exprNode()                             {}
-func (e *SliceExpr) Accept(v ExprVisitor) (any, error)     { return v.VisitSliceExpr(e) }
-func (e *SliceExpr) exprNode()                             {}
-func (e *KeyValueExpr) Accept(v ExprVisitor) (any, error)  { return v.VisitKeyValueExpr(e) }
-func (e *KeyValueExpr) exprNode()                          {}
-func (e *SliceType) Accept(v ExprVisitor) (any, error)     { return v.VisitSliceType(e) }
-func (e *SliceType) exprNode()                             {}
-func (e *StructType) Accept(v ExprVisitor) (any, error)    { return v.VisitStructType(e) }
-func (e *StructType) exprNode()                            {}
-func (e *InterfaceType) Accept(v ExprVisitor) (any, error) { return v.VisitInterfaceType(e) }
-func (e *InterfaceType) exprNode()                         {}
-func (e *MapType) Accept(v ExprVisitor) (any, error)       { return v.VisitMapType(e) }
-func (e *MapType) exprNode()                               {}
-func (e *FuncType) Accept(v ExprVisitor) (any, error)      { return v.VisitFuncType(e) }
-func (e *FuncType) exprNode()                              {}
-
 // --- statements ---
-type Stmt interface {
-	Accept(v StmtVisitor) error
-	stmtNode()
-}
-
 type PrintStmt struct {
 	Expr Expr
 }
@@ -269,41 +234,6 @@ type TypeDecl struct {
 	Name *Ident
 	Type Expr
 }
-
-func (s *PrintStmt) Accept(v StmtVisitor) error    { return v.VisitPrintStmt(s) }
-func (s *PrintStmt) stmtNode()                     {}
-func (s *ExprStmt) Accept(v StmtVisitor) error     { return v.VisitExprStmt(s) }
-func (s *ExprStmt) stmtNode()                      {}
-func (s *BlockStmt) Accept(v StmtVisitor) error    { return v.VisitBlockStmt(s) }
-func (s *BlockStmt) stmtNode()                     {}
-func (s *IfStmt) Accept(v StmtVisitor) error       { return v.VisitIfStmt(s) }
-func (s *IfStmt) stmtNode()                        {}
-func (s *ForStmt) Accept(v StmtVisitor) error      { return v.VisitForStmt(s) }
-func (s *ForStmt) stmtNode()                       {}
-func (s *ForRangeStmt) Accept(v StmtVisitor) error { return v.VisitForRangeStmt(s) }
-func (s *ForRangeStmt) stmtNode()                  {}
-func (s *ReturnStmt) Accept(v StmtVisitor) error   { return v.VisitReturnStmt(s) }
-func (s *ReturnStmt) stmtNode()                    {}
-func (s *BranchStmt) Accept(v StmtVisitor) error   { return v.VisitBranchStmt(s) }
-func (s *BranchStmt) stmtNode()                    {}
-func (s *IncDecStmt) Accept(v StmtVisitor) error   { return v.VisitIncDecStmt(s) }
-func (s *IncDecStmt) stmtNode()                    {}
-func (s *AssignStmt) Accept(v StmtVisitor) error   { return v.VisitAssignStmt(s) }
-func (s *AssignStmt) stmtNode()                    {}
-func (s *DeferStmt) Accept(v StmtVisitor) error    { return v.VisitDeferStmt(s) }
-func (s *DeferStmt) stmtNode()                     {}
-func (s *SwitchStmt) Accept(v StmtVisitor) error   { return v.VisitSwitchStmt(s) }
-func (s *SwitchStmt) stmtNode()                    {}
-func (s *CaseClause) Accept(v StmtVisitor) error   { return v.VisitCaseClause(s) }
-func (s *CaseClause) stmtNode()                    {}
-func (s *VarDecl) Accept(v StmtVisitor) error      { return v.VisitVarDecl(s) }
-func (s *VarDecl) stmtNode()                       {}
-func (s *ConstDecl) Accept(v StmtVisitor) error    { return v.VisitConstDecl(s) }
-func (s *ConstDecl) stmtNode()                     {}
-func (s *FuncDecl) Accept(v StmtVisitor) error     { return v.VisitFuncDecl(s) }
-func (s *FuncDecl) stmtNode()                      {}
-func (s *TypeDecl) Accept(v StmtVisitor) error     { return v.VisitTypeDecl(s) }
-func (s *TypeDecl) stmtNode()                      {}
 
 type Field struct {
 	Name *Ident
